b779cacbf11e4d8157c6d6ecc8e605bbbe7b12b5
Jakub Pavlík <severus@post.cz>
2017-11-07 16:59:49 +0100

    fix homepage path

--- gregoplus/urls.py
+++ gregoplus/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^home/', include('home.urls')),
+    url(r'^$', include('home.urls')),
     url(r'^scores/', include('scores.urls')),
     # url(r'^admin/', admin.site.urls),
 ]
