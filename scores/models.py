596e4c28bd57f1b7544e761a19e89c8ed0dceb92
Jakub Pavlík <severus@post.cz>
2017-11-07 17:07:50 +0100

    Usage: group and order by liturgical books

--- scores/models.py
+++ scores/models.py
@@ -3,24 +3,30 @@ from django.db import models
 class Score(models.Model):
     # see https://github.com/gregorio-project/GregoBase/blob/master/include/txt.php
     OFFICE_PART_CHOICES = (
-        ('al', 'Alleluia'),
-        ('an', 'Antiphona'),
-        ('ca', 'Canticum'),
-        ('co', 'Communio'),
-        ('gr', 'Graduale'),
-        ('hy', 'Hymnus'),
-        ('im', 'Improperia'),
-        ('in', 'Introitus'),
-        ('ky', 'Kyriale'),
-        ('of', 'Offertorium'),
-        ('or', 'Toni Communes'),
-        ('pr', 'Praefationes in tono solemni'),
-        ('ps', 'Offertorium'),
-        ('rb', 'Responsorium breve'), # "breve" is correct
-        ('re', 'Responsorium'),
-        ('se', 'Sequentia'),
-        ('tr', 'Tractus'),
-        ('va', 'Varia'),
+        ('Mass', (
+            ('in', 'Introitus'),
+            ('ky', 'Kyriale'),
+            ('gr', 'Graduale'),
+            ('al', 'Alleluia'),
+            ('se', 'Sequentia'),
+            ('of', 'Offertorium'),
+            ('pr', 'Praefationes in tono solemni'),
+            ('ps', 'Offertorium'),
+            ('co', 'Communio'),
+            ('tr', 'Tractus'),
+        )),
+        ('Divine Office', (
+            ('an', 'Antiphona'),
+            ('ca', 'Canticum'),
+            ('hy', 'Hymnus'),
+            ('rb', 'Responsorium breve'),  # "breve" is correct
+            ('re', 'Responsorium'),
+        )),
+        ('Cetera', (
+            ('or', 'Toni Communes'),
+            ('im', 'Improperia'),
+            ('va', 'Varia'),
+        ))
     )
     version = models.CharField(max_length=128)
     incipit = models.CharField(max_length=256)
