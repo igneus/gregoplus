7e97fbf6be59af972ec3aea5a9e577b908e90d88
Jakub Pavlík <severus@post.cz>
2018-10-03 13:22:54 +0200

    simple homepage test

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,8 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+class HomeTest(TestCase):
+    def test_index(self):
+        client = Client()
+        response = client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Gregoplus')
