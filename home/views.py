5b84b26dc3c0988e357b9d424aee81f0042566d4
Jakub Pavlik <severus@post.cz>
2025-08-22 22:37:32 +0200

    homepage: two random Chants

--- home/views.py
+++ home/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
+from scores.models import Chant
+
+
 def index(request):
-    return render(request, 'home/index.html')
+    return render(request, 'home/index.html', {
+        'scores': Chant.objects.order_by('?')[:2]
+    })
