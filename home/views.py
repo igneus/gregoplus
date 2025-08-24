17d15520b3db07d9df7bb6b32146bcf10b17edf2
Jakub Pavlik <severus@post.cz>
2025-08-25 00:00:56 +0200

    homepage: editing, basic stats

--- home/views.py
+++ home/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 
-from scores.models import Chant
+from scores.models import Chant, Source
 
 
 def index(request):
     return render(request, 'home/index.html', {
-        'scores': Chant.objects.order_by('?')[:2]
+        'scores': Chant.objects.order_by('?')[:2],
+        'chant_count': Chant.objects.count(),
+        'source_count': Source.objects.count(),
     })
